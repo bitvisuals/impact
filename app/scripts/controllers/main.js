8f91f32e05bb9931cfa706855db84e0719b9fac6
Milosz <miloszhygen@gmail.com>
2013-05-28 14:40:02 +0200

    added compass

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,14 @@
 
 angular.module('impactApp')
   .controller('MainCtrl', function ($scope) {
+
+    
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+
   });
