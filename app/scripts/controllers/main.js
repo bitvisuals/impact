ff1c263ec8f121d1c25104016d28741396535645
Wallace Sidhrée <i@dreamyguy.com>
2013-05-29 10:40:10 +0200

    correct indentation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,8 @@
 angular.module('impactApp')
   .controller('MainCtrl', function ($scope, $http) {
 
-  	var artID = 172142;
-  	var url = "http://dev-branch1.api.abcnyheter.no/v1/article/index.php?service=ABCNYHETER&scope=MYDESK_ARTICLE_BANK&id="+ artID +"&callback=JSON_CALLBACK";
+    var artID = 172142;
+    var url = "http://dev-branch1.api.abcnyheter.no/v1/article/index.php?service=ABCNYHETER&scope=MYDESK_ARTICLE_BANK&id="+ artID +"&callback=JSON_CALLBACK";
 
     // var jsonpData = "";
 
@@ -15,58 +15,47 @@ angular.module('impactApp')
     ];
 
 
-  //   $http({method: 'GET', url: '/someUrl'}).
-	 //  success(function(data, status, headers, config) {
-	 //    // this callback will be called asynchronously
-	 //    // when the response is available
-	 //  }).
-	 //  error(function(data, status, headers, config) {
-	 //    // called asynchronously if an error occurs
-	 //    // or server returns response with an error status.
-	 // });
+    //  $http({method: 'GET', url: '/someUrl'}).
+    //  success(function(data, status, headers, config) {
+    //    // this callback will be called asynchronously
+    //    // when the response is available
+    //  }).
+    //  error(function(data, status, headers, config) {
+    //    // called asynchronously if an error occurs
+    //    // or server returns response with an error status.
+    //  });
 
-    
-	//  function jsonp_callback(data) {
-	//     // returning from async callbacks is (generally) meaningless
-	//     console.log("DATA: " +data.found);
-	// }
+    //  function jsonp_callback(data) {
+    //    // returning from async callbacks is (generally) meaningless
+    //    console.log("DATA: " +data.found);
+    //  }
 
+  $http.jsonp(url).success(function(data, status, headers, config) {
+      // what do I do here?
+      // console.log("DATA: " + data);
+      // $scope.status = status;
+      // $scope.data = data;
 
-	
+      // obj = JSON.parse(data);
 
+      // console.log(obj.count);
 
+      // $scope.data  = data.data;
+      console.log(data.articles);
 
-	$http.jsonp(url).success(function(data, status, headers, config) {
-        //what do I do here?
-        // console.log("DATA: " + data);
-  //       $scope.status = status;
-  //       $scope.data = data;
+      $scope.author = data.articles[0].author;
+      $scope.byline = data.articles[0].byline;
+      $scope.article_content = data.articles[0].content;
+      $scope.article_date = data.articles[0].date;
+      $scope.art_images = data.articles[0].images;
+      $scope.ingress = data.articles[0].ingress;
+      $scope.art_title = data.articles[0].title;
 
-  //       // obj = JSON.parse(data);
-
-		// console.log(obj.count);
-
-		 // $scope.data  = data.data;
-		console.log(data.articles);
-
-		$scope.author = data.articles[0].author;
- 		$scope.byline = data.articles[0].byline;
- 		$scope.article_content = data.articles[0].content; 
-		$scope.article_date = data.articles[0].date;
-		$scope.art_images = data.articles[0].images;
- 		$scope.ingress = data.articles[0].ingress;
-		$scope.art_title = data.articles[0].title;
-
-
-         
     }).
     error(function(data, status, headers, config) {
         $scope.error = true;
         // console.log(data.data.articles);
     });
-
-
-
 
   });
 
@@ -74,11 +63,11 @@ angular.module('impactApp')
 function FetchCtrl($scope, $http, $templateCache) {
   $scope.method = 'GET';
   $scope.url = 'http-hello.html';
- 
+
   $scope.fetch = function() {
     $scope.code = null;
     $scope.response = null;
- 
+
     $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
       success(function(data, status) {
         $scope.status = status;
@@ -89,7 +78,7 @@ function FetchCtrl($scope, $http, $templateCache) {
         $scope.status = status;
     });
   };
- 
+
   $scope.updateModel = function(method, url) {
     $scope.method = method;
     $scope.url = url;
